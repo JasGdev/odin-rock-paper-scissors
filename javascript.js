b02f690cb08f0183f02b12aed2c63c8cb2ce89d9
Jason <jason13.deng@gmail.com>
2026-01-14 11:18:59 +0900

    Finish UI, with end state

--- javascript.js
+++ javascript.js
@@ -8,46 +8,65 @@ function getComputerChoice(){
         return "scissors"
     }
 }
+let body = document.querySelector('body');
 
 
 
 function playGame(){
+    let gameOver = false;
     let humanScore = 0;
     let computerScore = 0;
     let display = document.createElement('div');
     body.appendChild(display);
 
     function playRound(humanChoice, computerChoice){
-    
-    
-    
         if (humanChoice == 'rock' && computerChoice == 'scissors' ||
             humanChoice == 'scissors' && computerChoice == 'paper' ||
             humanChoice == 'paper' && computerChoice == 'rock' 
         ){
-            humanScore++;
-            display.textContent = `human wins with ${humanChoice} over computers ${computerChoice},
+            ++humanScore;
+            if (humanScore == 5){
+                console.log(humanScore)
+                display.textContent = 'GAME OVER, human is the ultimate winner,'+ ` human score is ${humanScore} 
+                and computer score is ${computerScore}`
+                gameOver = true;
+                console.log(gameOver)
+                
+            } 
+            else {
+                display.textContent = `human wins with ${humanChoice} over computers ${computerChoice},
                 human score is ${humanScore} and computer score is ${computerScore}`;
+            }
+            
         } else if (
             computerChoice == 'rock' && humanChoice == 'scissors' ||
             computerChoice == 'scissors' && humanChoice == 'paper' ||
             computerChoice == 'paper' && humanChoice == 'rock' 
         ){
-            computerScore++;
-            display.textContent = `computer wins with ${computerChoice} over humans ${humanChoice},
+            ++computerScore;
+            if (computerScore == 5){
+                console.log(computerScore)
+                display.textContent = 'GAME OVER, computer is the ultimate winner,'+ ` human score is ${humanScore}
+                and computer score is ${computerScore}`
+                gameOver = true;
+                console.log(gameOver)
+            } 
+            else {
+                display.textContent = `computer wins with ${computerChoice} over humans ${humanChoice},
                 human score is ${humanScore} and computer score is ${computerScore}`;
+            }
+
+            
         } else {
             display.textContent = `Tie, human score is ${humanScore} and computer score is ${computerScore}`;
         }
     }
 
-
     // button setup
-    let body = document.querySelector('body');
     body.addEventListener('click', (event) => {
         let target = event.target;
-
-        switch(target.id){
+        if (!gameOver){
+            switch(target.id){
             case 'rock':
                 playRound('rock', getComputerChoice());
                 // console.log("rock");
@@ -63,21 +82,10 @@ function playGame(){
                 // console.log("scissors");
                 break         
         }
+        } 
+        
     })
 
-    
-    
-
-    
-
-    // if (humanScore > computerScore){
-    //     console.log('human is the ultimate winner,'+ ` human score is ${humanScore} and computer score is ${computerScore}`)
-    // } else if (computerScore > humanScore){
-    //     console.log('computer is the ultimate winner,'+ ` human score is ${humanScore} and computer score is ${computerScore}`)
-    // } else {
-    //     console.log(`Tie, human score is ${humanScore} and computer score is ${computerScore}XXX`)
-    // }
-
-}
+    }
 
 playGame();
